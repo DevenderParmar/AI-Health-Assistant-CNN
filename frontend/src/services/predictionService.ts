9083fbe994f20eab42638dc610c271f501e6e5c0
DevenderParmar <devparmar2906@gmail.com>
2026-07-22 20:16:36 +0530

    Refactor project structure and prepare for deployment

--- frontend/src/services/predictionService.ts
+++ frontend/src/services/predictionService.ts
@@ -5,7 +5,7 @@ import type {
 } from "../types/prediction";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const predictDiabetes = async (
