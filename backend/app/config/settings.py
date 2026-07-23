5323076e894818252583e75e7de0c91b6a355296
DevenderParmar <devparmar2906@gmail.com>
2026-07-23 18:29:38 +0530

    Configure Docker and Docker Compose

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[3]
+# backend/
+BASE_DIR = Path(__file__).resolve().parents[2]
 
-MODEL_PATH = BASE_DIR / "model" / "saved_models" / "diabetes_ann.keras"
-SCALER_PATH = BASE_DIR / "model" / "saved_models" / "scaler.pkl"
+MODEL_PATH = BASE_DIR / "ml_models" / "diabetes_ann.keras"
+SCALER_PATH = BASE_DIR / "ml_models" / "scaler.pkl"
