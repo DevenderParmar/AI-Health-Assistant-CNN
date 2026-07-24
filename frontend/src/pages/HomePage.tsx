29e3ab5d2ab177038d7d833f4db347d4531b3df6
DevenderParmar <devparmar2906@gmail.com>
2026-07-24 19:17:40 +0530

    Update favicon and metadata

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,4 @@
-// import PredictionForm from "../components/prediction/PredictionForm";
-// import PredictionResult from "../components/prediction/PredictionResult";
 
-// function HomePage() {
-//   return (
-//     <main className="mx-auto flex min-h-[80vh] w-full max-w-7xl flex-col items-start justify-center gap-8 px-6 py-10 lg:flex-row lg:items-start">
-//       <PredictionForm />
-//       <PredictionResult />
-//     </main>
-//   );
-// }
-
-// export default HomePage;
 import { useState } from "react";
 import PredictionForm from "../components/prediction/PredictionForm";
 import PredictionResult from "../components/prediction/PredictionResult";
